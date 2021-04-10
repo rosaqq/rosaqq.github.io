ef0ffe5205f3b82ed9e43c4d815296bc31bef84f
Tiago Rosa <tcrosa27@gmail.com>
2021-04-10 12:33:32 +0100

    refactored and started using purgecss

--- compile.sh
+++ compile.sh
@@ -3,3 +3,5 @@
 # because I couldn't get WebStorm to run a python script
 # yes I am dumb maybe
 python compile.py
+# take out the trash
+purgecss --css css/main.css --content index.html --output css/
